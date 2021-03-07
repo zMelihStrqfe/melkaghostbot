4d49142a60752f8d909d512baafa932c8cc8e4a3
zMelihStrqfe <72662544+zMelihStrqfe@users.noreply.github.com>
2021-03-07 15:28:08 +0300

    Update bayrak.js

--- komutlar/bayrak.js
+++ komutlar/bayrak.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
  message.channel.send({embed: {
           file:"https://media.giphy.com/media/yDm4Ry6XU77Py/giphy.gif",
           color: 0xD97634,
-          description: "**:flag_tr: Türk bayrağı !**"
+          description: "**:flag_tr: işte anlı şanlı Türk bayrağımız!**"
             }});
 };
 
